e725d1ca764c52999ecce9f5831c0ed17d886c83
sonam wangdus <144471374+WangdusGitHub@users.noreply.github.com>
2026-01-28 12:57:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import useFetch from './Hooks/useFetch'
 import './App.css'
-import Card from './components/card';
+import Card from './components/Card';
 import { useEffect } from 'react';
 
 const url = "https://api.escuelajs.co/api/v1/products";
