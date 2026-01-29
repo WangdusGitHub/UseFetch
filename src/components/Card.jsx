7540fb31c89420b87a6d42d62b3bfb3d0989db12
sonam wangdus <144471374+WangdusGitHub@users.noreply.github.com>
2026-01-29 12:15:06 +0530

    change cars container from flex to grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,21 @@ function Card({item}) {
     const id = item.id;
     const title = item.title.length > 10 ? item.title.slice(0, 10) + "..." : item.title;
     const description = item.description.length > 40 ? item.description.slice(0,40) + "..." : item.description;
-    const price = item.price;
+    // const price = item.price;
     const images = item.images;
+
+    const getPrice = (price) => {
+        if(price >= 1000 && price < 1000000) {
+            return (price/1000).toFixed(2) + "k";
+        } else if(price >= 1000000 && price < 1000000000) {
+            return (price/1000000).toFixed(2) + "M";
+        } else if(price > 1000000000) {
+            return (price/1000000000).toFixed(2) + "B";
+        }
+        return price;
+    }
+
+    const price = getPrice(item.price);
 
     // console.log({item})
     return (
